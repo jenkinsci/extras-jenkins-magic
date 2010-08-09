1f69d4eb5e461ca6d17c174e60950b6f9373885b
capn_carrot <capn_carrot@@java.net>
2010-08-09 14:09:34 +0000

    copy files from work repo

---
+++ collect-from-personal.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Copy files from work repo
+
+cp -v ../../../../ups/trunk/bin/hudson/3party/pclint2checkstyle.xsl    static
+cp -v ../../../../ups/trunk/bin/hudson/3party/bullseye2cobertura.xsl   coverage
+cp -v ../../../../ups/trunk/bin/hudson/bin/gcovr.py                    coverage
+
+
